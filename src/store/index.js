e0b34e810b3f331d1f13e9eea5e682e34c4cf540
HAHA <1497154694@qq.com>
2021-02-23 11:29:01 +0800

    配置vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import rootModule from "./rootModule";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+const files = require.context("./modules", false, /\.js$/);
+
+files.keys().forEach(key => {
+  // ["./article.js", "./user.js"]
+  let moduleName = key.replace(/\.\//, "").replace(/\.js/, "");
+  let store = files(key).default;
+  let module = (rootModule.modules = rootModule.modules || {});
+  module[moduleName] = store;
+  // 命名空间
+  module[moduleName].namespaced = true;
 });
+
+export default new Vuex.Store(rootModule);
---
+++ src/store/rootModule.js
@@ -0,0 +1,7 @@
+export default {
+  state: {
+    bannerList: []
+  },
+  mutations: {},
+  actions: {}
+};
