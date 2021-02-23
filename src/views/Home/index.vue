70f2ff1dd3d4589fc045bb674fa7aa3dd6eebcf1
HAHA <1497154694@qq.com>
2021-02-23 17:24:11 +0800

    布局轮播图

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    首页
+    <el-carousel trigger="click" height="250px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
@@ -10,4 +14,20 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
