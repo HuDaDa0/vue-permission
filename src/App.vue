42ab147001a29f5c7a60342e18338f37320cc15f
HAHA <1497154694@qq.com>
2021-02-23 10:44:26 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <el-container>
+      <el-header>
+        <PageHeader></PageHeader>
+      </el-header>
+
+      <el-main>
+        <router-view />
+      </el-main>
+
+      <el-footer>
+        <PageFooter></PageFooter>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import PageHeader from "@/components/PageHeader";
+import PageFooter from "@/components/PageFooter";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: "App",
+  components: {
+    PageHeader,
+    PageFooter
   }
+};
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+.el-header,
+.el-footer {
+  color: #fff;
+  background-color: #20232a;
+}
+.el-main {
+  min-height: calc(100vh - 120px);
 }
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="20" class="header-nav-left">
+        <img alt="Vue logo" src="@/assets/logo.png" class="logo" />
+        <el-menu
+          :router="true"
+          mode="horizontal"
+          background-color="#20232a"
+          text-color="#fff"
+          active-text-color="#1890FF"
+        >
+          <el-menu-item index="/">首页</el-menu-item>
+          <el-menu-item index="/post">发表文章</el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="4">
+        <el-menu
+          :router="true"
+          mode="horizontal"
+          background-color="#20232a"
+          text-color="#fff"
+          active-text-color="#1890FF"
+          style="display: flex; justify-content: flex-end;"
+        >
+          <el-menu-item index="/login">登录</el-menu-item>
+          <el-menu-item index="/reg">注册</el-menu-item>
+          <el-submenu v-if="false">
+            <template slot="title">用户名</template>
+            <el-menu-item index="/logout">退出登录</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  width: 50px;
+  margin-top: 5px;
+}
+.header-nav-left {
+  display: flex;
+}
+</style>
