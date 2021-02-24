d22d7476528b57ee191a1bfc8f8c5c9f9ed0fe77
HAHA <1497154694@qq.com>
2021-02-24 10:10:51 +0800

    布局login页面

---
+++ src/views/User/login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="m-login">
+    <h3>登录</h3>
+    <div class="login-input-box">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+      >
+        <el-form-item label="用户名" prop="name">
+          <el-input
+            v-model="ruleForm.name"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            v-model="ruleForm.password"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">
+            登录
+          </el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      ruleForm: {
+        name: "",
+        password: ""
+      },
+      rules: {
+        name: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.m-login {
+  h3 {
+    text-align: center;
+  }
+  .login-input-box {
+    margin: 20px auto;
+    width: 500px;
+  }
+}
+</style>
