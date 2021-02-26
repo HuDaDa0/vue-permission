5cf4304025930c1bc924efbdaf417c3c60fb6b97
HAHA <1497154694@qq.com>
2021-02-26 15:44:03 +0800

    添加注释

--- src/router/hooks.js
+++ src/router/hooks.js
@@ -10,6 +10,7 @@ export default {
   },
   loginPermission: async function(to, from, next) {
     // 返回的结果存放到 vuex 中
+    // whiteList 白名单 可以配置哪些页面不需要验证的
     await store.dispatch("user/setUserValidate");
     next();
   }
