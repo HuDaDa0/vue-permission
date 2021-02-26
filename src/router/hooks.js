d938e9a118b112f84641faefbea694c962ef6d85
HAHA <1497154694@qq.com>
2021-02-26 15:50:51 +0800

    导航鉴权

--- src/router/hooks.js
+++ src/router/hooks.js
@@ -12,6 +12,20 @@ export default {
     // 返回的结果存放到 vuex 中
     // whiteList 白名单 可以配置哪些页面不需要验证的
     await store.dispatch("user/setUserValidate");
-    next();
+
+    if (store.state.user.hasPermission) {
+      if (to.path === "/login") {
+        next("/");
+      } else {
+        next();
+      }
+    } else {
+      // 看看哪些接口需要权限访问
+      let needLogin = to.matched.some(item => item.meta.needLogin);
+      if (needLogin) {
+        next("/login");
+      }
+      next();
+    }
   }
 };
--- src/router/routers/article.router.js
+++ src/router/routers/article.router.js
@@ -2,6 +2,9 @@ export default [
   {
     path: "/post",
     component: () =>
-      import(/* webpackChunkName: 'post' */ "@/views/Article/post.vue")
+      import(/* webpackChunkName: 'post' */ "@/views/Article/post.vue"),
+    meta: {
+      needLogin: true
+    }
   }
 ];
